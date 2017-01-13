4e46d768406e37de1017dbd5c83c4e6e59208ffe
hykw <hykw1234@gmail.com>
2017-01-13 11:07:02 +0900

    distinct: sign_in() OK/NG patterns

--- specimen/web/controllers/page_controller.ex
+++ specimen/web/controllers/page_controller.ex
@@ -4,8 +4,17 @@ defmodule Specimen.PageController do
   def index(conn, _params) do
     user = %Specimen.User{}
     perms = %{user: [:normal_login]}
-    conn = Guardian.Plug.sign_in(conn, user, :token, perms: perms)
+
+    # OK
+    Guardian.Plug.sign_in(conn, user, :token)
+    Guardian.Plug.sign_in(conn, user, :token, %{})
+
+
+    # NG
+    Guardian.Plug.sign_in(conn, user, :token, [])
+    Guardian.Plug.sign_in(conn, user, :token, perms: perms)
 
     render conn, "index.html"
   end
+
 end
